34c8830ffa65335f525de59f42c9ce19a4712829
Carlos Henriqeu da S. Moreira <kmoreira620@gmail.com>
2020-11-18 19:33:16 -0300

    Removes optional env variable

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,7 +4,6 @@ module.exports = ({ env }) => ({
     providerOptions: {
       account: env("STORAGE_ACCOUNT"),
       accountKey: env("STORAGE_ACCOUNT_KEY"),
-      serviceBaseURL: env("STORAGE_URL"),
       containerName: env("STORAGE_CONTAINER_NAME"),
       defaultPath: "assets",
       maxConcurrent: 10,
