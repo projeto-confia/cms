f2fa38b0e059d6e4e702a4260732eff550109157
Carlos Henrique da S. Moreira <kmoreira620@gmail.com>
2021-04-27 22:41:23 -0300

    Updates upload config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "azure-storage",
+    provider: 'cloudinary',
     providerOptions: {
-      account: env("STORAGE_ACCOUNT"),
-      accountKey: env("STORAGE_ACCOUNT_KEY"),
-      containerName: env("STORAGE_CONTAINER_NAME"),
-      defaultPath: "assets",
-      maxConcurrent: 10,
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
     },
   },
 });
